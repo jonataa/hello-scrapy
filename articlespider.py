799ee6c30f57b4bc6d0a8b4863a11b2cfd76fb78
Jonata Weber <jonataweber@gmail.com>
2015-07-17 18:19:05 -0300

    Fix scrap images

--- articlespider.py
+++ articlespider.py
@@ -10,19 +10,18 @@ class ArticleSpider(scrapy.Spider):
 
   def parse_article(self, response):
     for article_html in response.css('#glb-materia'):       
-      article = {
+      yield {
         'url': response.url,
         'title': article_html.css('[itemprop=headline]::text').extract_first(),
         'description': article_html.css('[itemprop=description]::text').extract_first(),
         'author': article_html.css('[itemprop=author]::text').extract_first(),
         'body': article_html.css('[itemprop=articleBody] p::text').extract(),
         'imgs': self.get_images(article_html)
-      }
-      yield article
+      }      
 
   def get_images(self, article_html):    
     images = []    
-    for img in article_html.css('img'):
+    for img in article_html.css('.foto img'):
       images.append({
         'src': img.xpath('@src').extract_first(), 
         'title': img.xpath('@title').extract_first()
